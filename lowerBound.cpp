d5fa9dd9a15dfe680cf4e483948ff2beabbab624
Aayush Mistri <153304770+Aayush-Mistri@users.noreply.github.com>
2024-09-16 00:07:43 +0530

    lower bound problem

---
+++ lowerBound.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int lowerBound(int arr[] , int target , int n){
+    int low = 0 , high = n - 1;
+    int mid;
+    int ans = n;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if(arr[mid] >= target){
+            ans = arr[mid];
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    
+}
+int main(){
+    int arr[5] = { 2 , 4 , 7 , 8 , 10 };
+    int n = 5;
+    int lb = 5;
+    int ans = lowerBound(arr , lb , n);
+    cout<<"The lower bound for following array is :"<<ans<<endl;
+return 0;
+}
